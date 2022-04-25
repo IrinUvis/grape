b3ed1ea1a481da46536205436c0769156f868ca5
IrinUvis <73426603+IrinUvis@users.noreply.github.com>
2022-04-25 14:21:01 +0200

    Theming setup (#4)
    * Migration to Material 3 (alpha) + adding custom theme + support for dynamic Material You colors.
    
    * Fixed detekt & lint issues

--- app/src/main/java/uvis/irin/grape/theme/Type.kt
+++ app/src/main/java/uvis/irin/grape/theme/Type.kt
@@ -1,28 +1,193 @@
+@file:Suppress("MagicNumber")
+
 package uvis.irin.grape.theme
 
-import androidx.compose.material.Typography
+import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import uvis.irin.grape.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+val Urbanist = FontFamily(
+    Font(
+        resId = R.font.urbanist_regular,
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+    Font(
+        resId = R.font.urbanist_italic,
+        style = FontStyle.Italic,
+    ),
+    Font(
+        resId = R.font.urbanist_extralight,
+        weight = FontWeight.ExtraLight,
+    ),
+    Font(
+        resId = R.font.urbanist_extralight_italic,
+        weight = FontWeight.ExtraLight,
+        style = FontStyle.Italic,
+    ),
+    Font(
+        resId = R.font.urbanist_light,
+        weight = FontWeight.Light,
+    ),
+    Font(
+        resId = R.font.urbanist_light_italic,
+        weight = FontWeight.Light,
+        style = FontStyle.Italic,
+    ),
+    Font(
+        resId = R.font.urbanist_semibold,
+        weight = FontWeight.SemiBold,
+    ),
+    Font(
+        resId = R.font.urbanist_semibold_italic,
+        weight = FontWeight.SemiBold,
+        style = FontStyle.Italic,
+    ),
+    Font(
+        resId = R.font.urbanist_bold,
+        weight = FontWeight.Bold,
+    ),
+    Font(
+        resId = R.font.urbanist_bold_italic,
+        weight = FontWeight.Bold,
+        style = FontStyle.Italic,
+    ),
+    Font(
+        resId = R.font.urbanist_extrabold,
+        weight = FontWeight.ExtraBold,
+    ),
+    Font(
+        resId = R.font.urbanist_extrabold,
+        weight = FontWeight.ExtraBold,
+        style = FontStyle.Italic,
+    ),
+    Font(
+        resId = R.font.urbanist_medium,
+        weight = FontWeight.Medium,
+    ),
+    Font(
+        resId = R.font.urbanist_medium_italic,
+        weight = FontWeight.Medium,
+        style = FontStyle.Italic,
+    ),
+    Font(
+        resId = R.font.urbanist_black,
+        weight = FontWeight.Black,
+    ),
+    Font(
+        resId = R.font.urbanist_black_italic,
+        weight = FontWeight.Black,
+        style = FontStyle.Italic,
+    ),
+)
+
+val AppTypography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = Urbanist,
+        fontWeight = FontWeight.W400,
+        fontSize = 57.sp,
+        lineHeight = 64.sp,
+        letterSpacing = (-0.25).sp,
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Urbanist,
+        fontWeight = FontWeight.W400,
+        fontSize = 45.sp,
+        lineHeight = 52.sp,
+        letterSpacing = 0.sp,
+    ),
+    displaySmall = TextStyle(
+        fontFamily = Urbanist,
+        fontWeight = FontWeight.W400,
+        fontSize = 36.sp,
+        lineHeight = 44.sp,
+        letterSpacing = 0.sp,
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = Urbanist,
+        fontWeight = FontWeight.W400,
+        fontSize = 32.sp,
+        lineHeight = 40.sp,
+        letterSpacing = 0.sp,
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = Urbanist,
+        fontWeight = FontWeight.W400,
+        fontSize = 28.sp,
+        lineHeight = 36.sp,
+        letterSpacing = 0.sp,
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = Urbanist,
+        fontWeight = FontWeight.W400,
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+        letterSpacing = 0.sp,
+    ),
+    titleLarge = TextStyle(
+        fontFamily = Urbanist,
+        fontWeight = FontWeight.W400,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp,
+    ),
+    titleMedium = TextStyle(
+        fontFamily = Urbanist,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.1.sp,
+    ),
+    titleSmall = TextStyle(
+        fontFamily = Urbanist,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.1.sp,
+    ),
+    labelLarge = TextStyle(
+        fontFamily = Urbanist,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.1.sp,
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Urbanist,
+        fontWeight = FontWeight.W400,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp,
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Urbanist,
+        fontWeight = FontWeight.W400,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp,
+    ),
+    bodySmall = TextStyle(
+        fontFamily = Urbanist,
+        fontWeight = FontWeight.W400,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.4.sp,
+    ),
+    labelMedium = TextStyle(
+        fontFamily = Urbanist,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp,
+    ),
+    labelSmall = TextStyle(
+        fontFamily = Urbanist,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp,
+    ),
 )
