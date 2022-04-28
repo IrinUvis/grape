b5a361b170bc81c964c1f2f43df2920e688d8f0d
IrinUvis <k.kowalski2001@outlook.com>
2022-04-28 23:55:45 +0200

    Added edge to edge experience to the app

--- app/src/main/java/uvis/irin/grape/MainActivity.kt
+++ app/src/main/java/uvis/irin/grape/MainActivity.kt
@@ -3,19 +3,42 @@ package uvis.irin.grape
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.core.view.WindowCompat
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import uvis.irin.grape.core.ui.theme.GrapeTheme
 import uvis.irin.grape.soundlist.ui.SoundListScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
             GrapeTheme {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    SoundListScreen()
+                val systemUiController = rememberSystemUiController()
+                val useDarkIcons = !isSystemInDarkTheme()
+
+                SideEffect {
+                    // Update all of the system bar colors to be transparent, and use
+                    // dark icons if we're in light theme
+                    systemUiController.setSystemBarsColor(
+                        color = Color.Transparent,
+                        darkIcons = useDarkIcons
+                    )
+                }
+
+                ProvideWindowInsets {
+                    Surface(modifier = Modifier.fillMaxSize()) {
+                        SoundListScreen()
+                    }
                 }
             }
         }
--- app/src/main/java/uvis/irin/grape/soundlist/ui/SoundListContent.kt
+++ app/src/main/java/uvis/irin/grape/soundlist/ui/SoundListContent.kt
@@ -9,20 +9,21 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.Scaffold
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.google.accompanist.insets.LocalWindowInsets
+import com.google.accompanist.insets.rememberInsetsPaddingValues
+import uvis.irin.grape.core.ui.components.TopAppBar
 import uvis.irin.grape.core.ui.theme.GrapeTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SoundListContent() {
     Scaffold(
@@ -33,7 +34,6 @@ fun SoundListContent() {
                 .fillMaxSize()
                 .padding(paddingValues)
         ) {
-
             Card {
                 Text(text = "Hello Grape!")
             }
@@ -43,9 +43,16 @@ fun SoundListContent() {
 
 @Composable
 fun SoundSectionTabBar() {
-    TopAppBar {
+    TopAppBar(
+        contentPadding = rememberInsetsPaddingValues(
+            LocalWindowInsets.current.statusBars,
+            applyBottom = false,
+        ),
+    ) {
         Row(
-            modifier = Modifier.horizontalScroll(rememberScrollState())
+            modifier = Modifier
+                .padding(4.dp)
+                .horizontalScroll(rememberScrollState())
         ) {
             SoundSectionTab(name = "Stonoga", false)
             SoundSectionTab(name = "Jagoda", false)
