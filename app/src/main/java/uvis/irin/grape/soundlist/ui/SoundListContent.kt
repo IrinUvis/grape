8aca8f81a275e2147e980f33e635ec9eab18d896
IrinUvis <irinuvis.dev@gmail.com>
2022-05-30 11:05:51 +0200

    KtLint format

--- app/src/main/java/uvis/irin/grape/soundlist/ui/SoundListContent.kt
+++ app/src/main/java/uvis/irin/grape/soundlist/ui/SoundListContent.kt
@@ -22,8 +22,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.ScrollableTabRow
-import androidx.compose.material3.Snackbar
-import androidx.compose.material3.SnackbarData
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
--- app/src/main/java/uvis/irin/grape/soundlist/ui/SoundListViewModel.kt
+++ app/src/main/java/uvis/irin/grape/soundlist/ui/SoundListViewModel.kt
@@ -13,7 +13,6 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import uvis.irin.grape.BuildConfig
