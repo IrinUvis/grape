ff5ff3bddbada1b02e196509e499b7cbde0c56a3
IrinUvis <irinuvis@gmail.com>
2022-09-25 22:23:12 +0200

    Display favourites toggle

--- app/src/main/java/uvis/irin/grape/soundlist/ui/SoundListContent.kt
+++ app/src/main/java/uvis/irin/grape/soundlist/ui/SoundListContent.kt
@@ -24,7 +24,6 @@ import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsBottomHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
